4d3e18a6b6078bff1193ca3188d53cd60a82fde9
will@2012 <xibaow2020@qq.com>
2024-05-30 12:26:19 +0800

    fix: log print

--- trie/trie_reader.go
+++ trie/trie_reader.go
@@ -72,7 +72,7 @@ func (r *trieReader) node(path []byte, hash common.Hash) ([]byte, error) {
 	context := []interface{}{}
 	blob, err := r.reader.Node(r.owner, path, hash, context)
 	if time.Now().Sub(start) > 10*time.Millisecond {
-		log.Info("Slow query trie node", context)
+		log.Info("Slow query trie node", context...)
 	}
 	if err != nil || len(blob) == 0 {
 		return nil, &MissingNodeError{Owner: r.owner, NodeHash: hash, Path: path, err: err}

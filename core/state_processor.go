a05d32c647a28e626f7dd1ccda9770e7b01bbc2e
2020xibao <117156346+will-2012@users.noreply.github.com>
2025-06-17 09:19:12 +0800

    chore: add out snap metrics

--- core/state_processor.go
+++ core/state_processor.go
@@ -72,6 +72,11 @@ func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg
 		gp          = new(GasPool).AddGas(block.GasLimit())
 	)
 
+	statedb.EnablePerf = true
+	defer func() {
+		statedb.EnablePerf = false
+	}()
+
 	// Mutate the block and state according to any hard-fork specs
 	if p.config.DAOForkSupport && p.config.DAOForkBlock != nil && p.config.DAOForkBlock.Cmp(block.Number()) == 0 {
 		misc.ApplyDAOHardFork(statedb)

b344d4236d4248357193c9666c8b1f4c30b5b31b
2020xibao <117156346+will-2012@users.noreply.github.com>
2025-07-05 11:20:11 +0800

    chore: debug log

--- core/state/state_object.go
+++ core/state/state_object.go
@@ -232,6 +232,7 @@ func (s *stateObject) GetCommittedState(key common.Hash) common.Hash {
 
 	if value, cached := s.tryGetFromSharedPool(key); cached {
 		s.originStorage[key] = value
+		log.Info("tryGetFromSharedPool: true", "key", key, "value", value)
 		return value
 	}
 
